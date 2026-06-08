9a191891cd97ccfc0638c19f39c3a22683dded94
Noixelfer <pauldarius98@gmail.com>
2026-06-08 23:44:28 +0300

    Feat: Github pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://noixelfer.github.io",
+  base: "/unity-portfolio",
   vite: {
     plugins: [tailwindcss()],
   },
